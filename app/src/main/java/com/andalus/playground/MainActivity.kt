9f992ba8b56562e07ce5a77f62384c2b0fce8709
Abo_Med7at <abomed7at55@gmail.com>
2019-10-19 22:00:13 +0200

    Completed the very basics binding

--- app/src/main/java/com/andalus/playground/MainActivity.kt
+++ app/src/main/java/com/andalus/playground/MainActivity.kt
@@ -2,11 +2,25 @@ package com.andalus.playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.andalus.playground.databinding.ActivityMainBinding
+import com.andalus.playground.models.User
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val userBinding  = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        val customUser = User("Mohamed","mohamed.medhat0298@gmail.com")
+
+        userBinding.mainActivityUser = customUser
+
+        btnChange.setOnClickListener {
+            customUser.name = etName.text.toString()
+            customUser.email = etEmail.text.toString()
+            userBinding.mainActivityUser = customUser
+        }
     }
 }
---
+++ app/src/main/java/com/andalus/playground/models/User.kt
@@ -0,0 +1,3 @@
+package com.andalus.playground.models
+
+class User(var name: String, var email: String)
